e98cf2aefabbdd5c5842130ef2873d9ba1f9b3ad
heckrodrigo <102565577+heckrodrigo@users.noreply.github.com>
2023-09-29 15:50:59 -0300

    Descrevendo a função do Script

--- app.js
+++ app.js
@@ -11,3 +11,11 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+/*
+Em resumo, o código está criando uma funcionalidade de controle de botões e 
+um botão para alternar entre modos claro e escuro em uma página da web. 
+Quando um botão de controle é clicado, ele altera as classes dos botões e 
+dos elementos correspondentes na página. O botão de alternância de tema muda 
+entre os modos claro e escuro, aplicando uma classe ao corpo do documento.*/
